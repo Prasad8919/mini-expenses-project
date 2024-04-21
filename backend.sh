f4c215d54330aa107fdbf324c344b1575b6ddc14
prasad.b <prasad@gmail.com>
2024-04-21 22:42:15 +0530

    some-message

--- backend.sh
+++ backend.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 USERID=$ (id -u )
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run the scrpit with root user."
+else
+   echo "you are the super user"
+fi
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2..$R FAILURE $N"
+      exit 1
+    else
+      echo -e "$2..$G SUCCESS $N"
+    fi       
+}
+
+
---
+++ my sql.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$ (id -u )
+
+echo "Please enter DB password:"
+read -s mysql_root_password
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run the scrpit with root user."
+else
+   echo "you are the super user"
+fi
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2..$R FAILURE $N"
+      exit 1
+    else
+      echo -e "$2..$G SUCCESS $N"
+    fi       
+}
+
+dnf install my sql -y &>>$LOGFILE
+VALIDATE $? "installing my sql"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+mysql -h 172.31.87.132 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
