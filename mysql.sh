db70ebd2f29e0ddc4d5fd56874d1c5e29a1e86c0
prasad.b <prasad@gmail.com>
2024-04-21 23:19:57 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+
 echo "Please enter DB password:"
 read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo -e "$2...$R FAILURE $N"
+        echo -e "FAILURE"
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "SUCCESS"
     fi
 }
 
@@ -30,23 +24,23 @@ else
 fi
 
 
-dnf install mysql-server -y &>>$LOGFILE
+dnf install mysql-server -y 
 VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld &>>$LOGFILE
+systemctl enable mysqld 
 VALIDATE $? "Enabling MySQL Server"
 
-systemctl start mysqld &>>$LOGFILE
+systemctl start mysqld 
 VALIDATE $? "Starting MySQL Server"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h 172.31.87.132 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.87.132 -uroot -p${mysql_root_password} -e 'show databases;' 
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} 
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
