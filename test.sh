c6fc0c7a78037798da97b665e84def99e33f232b
prasad.b <prasad@gmail.com>
2024-04-24 12:22:25 +0530

    commit-3

--- test.sh
+++ test.sh
@@ -12,3 +12,5 @@ number_of_words=`wc --word < $file_path`
 # Displaying number of lines and number of words
 echo "Number of lines: $number_of_lines"
 echo "Number of words: $number_of_words"
+
+cat file.txt | tr -cs '[:alpha:]' '\n' | tr 'A-Z' 'a-z' | sort | uniq -c
