1558356d58fd5d50f4081120be9fe36dd0d639f6
prasad.b <prasad@gmail.com>
2024-04-24 11:14:24 +0530

    commit-3

--- test.sh
+++ test.sh
@@ -3,12 +3,3 @@
 FILENAME=$file.txt
 
 cat $file.txt ilename |  sed "s/_/new/g" | sed "s/$word/_/g" | tr -c -d _ |wc -c
-
-
-echo "Enter the term"
-
-read term
-
-result=`grep -o $term file.txt | wc -l`
-
-echo $result
