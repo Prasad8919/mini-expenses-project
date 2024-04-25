3437bc311033357bf83a0ef5295c6ba8ebbbd9df
prasad.b <prasad@gmail.com>
2024-04-25 12:32:13 +0530

    commit-3

--- mysql.sh
+++ mysql.sh
@@ -2,27 +2,10 @@
 
 USERID=$(id -u)
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo "$2...FAILURE"
-        exit 1
-    else
-        echo "$2...SUCCESS"
-    fi
-}
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+if[ $USERID -ne 0 ]
+  print "please run this script with root access"
 else
-    echo "You are super user."
-fi
-
-dnf install mysql -y 
-VALIDATE $? "Installing MySQL"
-
-dnf install git -y
-VALIDATE $? "Installing Git"
+ print "your are the super user"
+fi 
 
