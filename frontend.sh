ad197a358fcfdc1fafb269a44dd18f63dfc124b8
prasad.b <prasad@gmail.com>
2024-04-27 13:00:48 +0530

    mini-1

--- frontend.sh
+++ frontend.sh
@@ -46,7 +46,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/mini-expenses-project/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
